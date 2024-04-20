15a38c413cb3af4cab5a902867168ad8682e1226
0xreddy <0xreddy@gmail.com>
2024-04-20 18:25:29 +0530

    Update readme and improve intro/outro

--- src/Interface.jsx
+++ src/Interface.jsx
@@ -27,30 +27,7 @@ export default function Interface()
     
         {/* Intro / outro */}
         <div className={ `screen intro ${ status === 'intro' || status === 'outro' ? 'is-visible' : '' }` }>
-            <h1 className="title">Bounce Friday</h1>
-            <h2 className="sub-title">A <a href="https://threejs-journey.com" target="_blank">Three.js Journey</a> game by <a href="https://bruno-simon.com" target="_blank">Bruno Simon</a> for BLACK FRIDAY</h2>
-            <div className="description">
-                <div className="side is-left">
-                    <div className="sub-title">Presentation</div>
-                    <p>This platform game was created in less than 24 hours to demonstrate what can be built with Three.js.</p>
-                    <p>
-                        Interested in learning how to create this type of experience?
-                        <br /><a href="https://threejs-journey.com" target="_blank">Three.js Journey</a> is the most complete course you need!
-                    </p>
-                    <p>Get <a class="highlight" href="https://threejs-journey.com" target="_blank">50% OFF</a> today for <a href="https://threejs-journey.com" target="_blank">Black Friday</a>!</p>
-                </div>
-                <div className="side is-right">
-                    <div className="sub-title">Credits</div>
-                    <ul>
-                        <li><a href="https://threejs.org" target="_blank">Three.js</a></li>
-                        <li><a href="https://react.dev" target="_blank">React</a></li>
-                        <li><a href="https://docs.pmnd.rs/react-three-fiber" target="_blank">React Three Fiber</a></li>
-                        <li><a href="https://github.com/pmndrs/ecctrl/" target="_blank">Ecctrl</a></li>
-                        <li><a href="https://github.com/pmndrs/postprocessing" target="_blank">Postprocessing</a></li>
-                        <li>Find more on <a href="https://github.com/brunosimon/three.js-journey-2023-black-friday" target="_blank">GitHub</a></li>
-                    </ul>
-                </div>
-            </div>
+            <h1 className="title">Bounce </h1>
             <button ref={ startButton } className="start-button" onTouchStart={ onTouchStart } onClick={ onStartClick }>
                 <div className="video">
                     <video src="./videos/button-preview.mp4" muted autoPlay loop />
